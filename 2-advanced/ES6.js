3c65f57507281a96708dfdcc2e8f0f4ee9ac5a7a
peterdangvnngithub <peterdangvnn@gmail.com>
2022-12-17 11:32:01 +0700

    17/12/2022

---
+++ 2-advanced/ES6.js
@@ -0,0 +1,9 @@
+const x = 5;
+
+// 1. let, const
+function add() {
+	x = 10;
+}
+
+add();
+console.log(x);
--- index.js
+++ index.js
@@ -1,58 +1,19 @@
-// PROMISE
-
-function httpGetAsync(theUrl, resolve) {
-	var xmlHttp = new XMLHttpRequest();
-	xmlHttp.onreadystatechange = function () {
-		if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
-			resolve(xmlHttp);
-		}
-	};
-	xmlHttp.open('GET', theUrl, true); // true for asynchronous
-	xmlHttp.send(null);
-}
-
-const currentPromise = new Promise((resolve, reject) => {
-	httpGetAsync('https://picsum.photos/200/300', resolve);
+console.log('hi');
+var myPromise = new Promise(function (resolve, reject) {
+	if (true) {
+		resolve('There');
+	} else {
+		reject("Aww, didn't work");
+	}
 });
 
-currentPromise
-	.then((data) => {
-		console.log(data);
-	})
-	.catch((err) => {
-		console.log(err);
-	});
-
-//function test() {
-//setTimeout(() => console.log('1'), 0);
-//	console.log('2');
-//	console.log('3');
-//}
-
-//function httpGetAsync(theUrl, callback) {
-//	var xmlHttp = new XMLHttpRequest();
-//	xmlHttp.onreadystatechange = function () {
-//		if (xmlHttp.readyState == 4 && xmlHttp.status == 200) callback(xmlHttp);
-//	};
-//	xmlHttp.open('GET', theUrl, true); // true for asynchronous
-//	xmlHttp.send(null);
-//}
-
-//httpGetAsync('https://picsum.photos/200/300', (data) => {
-//	console.log(data);
-//	document.getElementById('img_1').setAttribute('src', data.responseURL);
-
-//	httpGetAsync('https://picsum.photos/200/300', (data) => {
-//		console.log(data);
-//		document.getElementById('img_2').setAttribute('src', data.responseURL);
-
-//		httpGetAsync('https://picsum.photos/200/300', (data) => {
-//			console.log(data);
-//			document
-//				.getElementById('img_3')
-//				.setAttribute('src', data.responseURL);
-//		});
-//	});
-//});
-
-//test();
+myPromise.then(
+	function (result) {
+		// Resolve callback
+		console.log(result);
+	},
+	function (result) {
+		// Reject callback
+		console.error(result);
+	}
+);
